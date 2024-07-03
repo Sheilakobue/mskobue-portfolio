e095f1571a68da755a7bd1ff082c60c717ef0b36
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-03 16:11:49 +0200

    created a hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import ParticlesBackground from "./ParticlesBackround";
+import { FaGithub, FaDev,FaFacebook,FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+
+
+export default function Hero() {
+  return (
+    <div>Hero</div>
+  )
+}
